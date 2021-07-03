afdf9ed36a135a0dc0221a5c63d423f562ad52a1
Chris Tralie <chris.tralie@gmail.com>
2021-07-02 20:33:09 -0400

    Round 1 of relative path fixes

--- replacePaths.py
+++ replacePaths.py
@@ -1,14 +1,15 @@
 import glob
 
-for origpath in ["http://webpages.ursinus.edu/nscoville/", "/nscoville/"]:
-    for f in glob.glob("**/*.html", recursive=True):
-        ndirs = len(f.split("/"))-1
-        fin = open(f, encoding="utf8", errors='ignore')
-        s = fin.read()
-        fin.close()
-        if origpath in s:
-            print(f)
-            s = s.replace(origpath, "../"*ndirs)
-            fout = open(f, "w")
-            fout.write(s)
-            fout.close()
+origpath = "http://webpages.ursinus.edu/nscoville/\""
+#origpath = "http://webpages.ursinus.edu/nscoville/"
+for f in glob.glob("**/*.html", recursive=True):
+    ndirs = len(f.split("/"))-1
+    fin = open(f, encoding="utf8", errors='ignore')
+    s = fin.read()
+    fin.close()
+    if origpath in s:
+        print(f)
+        s = s.replace(origpath, "../"*ndirs + "index.html\"")
+        fout = open(f, "w")
+        fout.write(s)
+        fout.close()
