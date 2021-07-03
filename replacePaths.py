cefdae18a11f86dd8b0b73383ce54104244b6cf0
Chris Tralie <chris.tralie@gmail.com>
2021-07-02 20:47:17 -0400

    Last round of URL updates

--- replacePaths.py
+++ replacePaths.py
@@ -1,14 +1,15 @@
 import glob
 
 #origpath = "http://webpages.ursinus.edu/nscoville/\""
-origpath = "http://webpages.ursinus.edu/nscoville/"
+#origpath = "http://webpages.ursinus.edu/nscoville/"
+origpath = "/nscoville/"
 for f in glob.glob("**/*.html", recursive=True):
     ndirs = len(f.split("/"))-1
     fin = open(f, encoding="utf8", errors='ignore')
     s = fin.read()
     fin.close()
     if origpath in s:
-        replace = "../"*ndirs
+        replace = ""#"../"*ndirs
         print(f, replace)
         s = s.replace(origpath, replace)
         fout = open(f, "w")
