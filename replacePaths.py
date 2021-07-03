6d043157ad450c8dba63a512e4a98872944b9a82
Chris Tralie <chris.tralie@gmail.com>
2021-07-02 20:40:43 -0400

    Link updates stage 2

--- replacePaths.py
+++ replacePaths.py
@@ -1,15 +1,16 @@
 import glob
 
-origpath = "http://webpages.ursinus.edu/nscoville/\""
-#origpath = "http://webpages.ursinus.edu/nscoville/"
+#origpath = "http://webpages.ursinus.edu/nscoville/\""
+origpath = "http://webpages.ursinus.edu/nscoville/"
 for f in glob.glob("**/*.html", recursive=True):
     ndirs = len(f.split("/"))-1
     fin = open(f, encoding="utf8", errors='ignore')
     s = fin.read()
     fin.close()
     if origpath in s:
-        print(f)
-        s = s.replace(origpath, "../"*ndirs + "index.html\"")
+        replace = "../"*ndirs
+        print(f, replace)
+        s = s.replace(origpath, replace)
         fout = open(f, "w")
         fout.write(s)
         fout.close()
